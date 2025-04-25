b6b39776cb85bb5b3b9e4685fc6e60610919e250
jpdsf <jpdsafg@gmail.com>
2025-04-25 02:10:16 -0300

    refactor: ajustar configuração do Docker e melhorar a estrutura do código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,16 @@ export default function HomePage() {
   }
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-blue-200 text-white">
+    <main className="min-h-screen flex bg-blue-200 text-white">
       <Container size="sm">
         <Paper
           p="xl"
           radius="md"
           shadow="md"
-          className="bg-white text-gray-900"
+          className="bg-white text-gray-900 mt-8"
         >
           <Title order={2} className="mb-4 text-center ">
-            🎉 Boas-vindas ao Passou Aqui!
+            🎉 Boas-vindas ao Passou Aquiiiiiiiiiiiiii!
           </Title>
           <Text size="md" className="text-center mb-6">
             Seu sistema de gerenciamento de produtos está pronto para evoluir.
