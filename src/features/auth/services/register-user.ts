d8a2a4ba9153a34ac5e93d32c36e5f164df0aea0
jpdsf <jpdsafg@gmail.com>
2025-05-01 19:25:46 -0300

    Remove unused files and configurations, including SVG assets, PostCSS configuration, global styles, and authentication components. Clean up the project structure by deleting unnecessary middleware, API services, and TypeScript configurations. This commit streamlines the codebase and prepares for a more focused development approach.

--- src/features/auth/services/register-user.ts
+++
@@ -1,8 +0,0 @@
-import { api } from '@/lib/api'
-import { AuthResponse, RegisterPayload } from '../types/auth-type'
-
-export async function registerUser(
-  payload: RegisterPayload,
-): Promise<AuthResponse> {
-  return await api<AuthResponse>('accounts/register/', 'POST', payload)
-}
