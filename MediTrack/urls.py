3e3a817fbff002056a30c19c83a25ed59cd0ad99
jpdsf <jpdsafg@gmail.com>
2025-05-03 19:07:40 -0300

    feat: adicionar campo 'tarja' ao modelo Drug e atualizar serializers e admin

--- MediTrack/urls.py
+++ MediTrack/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include # type: ignore
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/v1/admin/', admin.site.urls),
     path('api/v1/inventory/', include('inventory.urls')),
     path('api/v1/accounts/', include('accounts.urls')),
 
