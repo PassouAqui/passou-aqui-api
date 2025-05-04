fe6b847694e4ad3589ca00b7ce312dc6044a434b
jpdsf <jpdsafg@gmail.com>
2025-05-04 13:11:59 -0300

    feat: adicionar campo 'id' ao serializer Drug e incluir opções de tarja no Choices

--- inventory/serializers/drug_serializer.py
+++ inventory/serializers/drug_serializer.py
@@ -4,5 +4,5 @@ from inventory.models import Drug
 class DrugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
-        fields = ['nome', 'descricao', 'tag_uid', 'lote', 'validade', 'ativo', 'tarja']
+        fields = ['id','nome', 'descricao', 'tag_uid', 'lote', 'validade', 'ativo', 'tarja']
 
