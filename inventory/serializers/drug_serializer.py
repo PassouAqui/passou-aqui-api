ab5d572cc2081634187e60a79c043ca0e02240f5
jpdsf <jpdsafg@gmail.com>
2025-05-04 13:11:59 -0300

    feat: adicionar campo 'id' ao serializer Drug e incluir opções de tarja no Choices

--- inventory/serializers/drug_serializer.py
+++ inventory/serializers/drug_serializer.py
@@ -6,5 +6,9 @@ import logging
 class DrugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
+<<<<<<< HEAD
         fields = '__all__'
+=======
+        fields = ['id','nome', 'descricao', 'tag_uid', 'lote', 'validade', 'ativo', 'tarja']
+>>>>>>> fe6b847 (feat: adicionar campo 'id' ao serializer Drug e incluir opções de tarja no Choices)
 
--- inventory/utils/Choices.py
+++ inventory/utils/Choices.py
@@ -3,12 +3,15 @@ from django.utils.translation import gettext_lazy as _
 
 class TarjaChoices(models.TextChoices):
     SEM_TARJA = 'ST', _('Sem Tarja')
+<<<<<<< HEAD
     
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class TarjaChoices(models.TextChoices):
     SEM_TARJA = 'ST', _('Sem Tarja')
+=======
+>>>>>>> fe6b847 (feat: adicionar campo 'id' ao serializer Drug e incluir opções de tarja no Choices)
     TARJA_AMARELA = 'TA', _('Tarja Amarela')
     TARJA_VERMELHA = 'TV', _('Tarja Vermelha')
     TARJA_PRETA = 'TP', _('Tarja Preta')
