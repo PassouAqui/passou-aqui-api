c7ddb2343cc2538c70b40f6357832582bf3fd629
jpdsf <jpdsafg@gmail.com>
2025-06-17 22:34:17 -0300

    feat: enhance CORS and cookie settings, update authentication endpoints
    - Added additional CORS methods and headers to improve API security and flexibility.
    - Configured secure cookie settings for session and CSRF tokens based on the DEBUG flag.
    - Updated authentication endpoints to use cookie-based JWT authentication, replacing previous token-based views.
    - Refactored URLs in accounts and main application to align with new authentication methods.

--- MediTrack/urls.py
+++ MediTrack/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin 
 from django.urls import path, include 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView 
-
+from accounts.views.token_views import CookieTokenObtainPairView, CookieTokenRefreshView, LogoutView
 
 urlpatterns = [
     path('api/v1/admin/', admin.site.urls),
     path('api/v1/inventory/', include('inventory.urls')),
     path('api/v1/accounts/', include('accounts.urls')),
 
-
-    path('api/v1/auth/token', TokenObtainPairView.as_view()),
-    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view()),
-    path('api/v1/auth/token/verify/', TokenVerifyView.as_view()),
-
+    # Endpoints de autenticação
+    path('api/v1/auth/login/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth/logout/', LogoutView.as_view(), name='auth_logout'),
 ]
