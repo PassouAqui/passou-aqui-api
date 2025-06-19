bc27bb392576dc96f4a9c48fc3a4318b968bd2df
jpdsf <jpdsafg@gmail.com>
2025-06-19 18:11:17 -0300

    feat: adicionar modelo Records e endpoints para registro de leituras RFID

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,12 +4,13 @@ from inventory.views import LocationViewSet
 from inventory.views.cart_views import CartViewSet
 from inventory.views.drug_viewset import DrugViewSet
 from inventory.views.dashboard_view import DashboardView
+from inventory.views.records_view import RecordsViewSet
 
 router = DefaultRouter()
 router.register(r'drugs', DrugViewSet, basename='drug')
 router.register(r'locations', LocationViewSet)
 router.register(r'carts', CartViewSet)
-
+router.register(r'records', RecordsViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
