49c14757c7155132c748a9fe5cb0cd4f52a0ad68
jpdsf <jpdsafg@gmail.com>
2025-09-06 11:10:30 -0300

    feat: adicionar campo 'id' ao serializer Drug e incluir opções de tarja no Choices

--- inventory/utils/Choices.py
+++ inventory/utils/Choices.py
@@ -3,4 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 class TarjaChoices(models.TextChoices):
     SEM_TARJA = 'ST', _('Sem Tarja')
-    
+    TARJA_AMARELA = 'TA', _('Tarja Amarela')
+    TARJA_VERMELHA = 'TV', _('Tarja Vermelha')
+    TARJA_PRETA = 'TP', _('Tarja Preta')
