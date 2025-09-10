f0538fda54b111ede07db989ca377b973cfd3a32
jpdsf <jpdsafg@gmail.com>
2025-09-10 11:14:00 -0300

    feat: enhance DrugSerializer with validation and logging
    - Added validation for expiration date to ensure it is not in the past.
    - Implemented validation for 'tarja' to check against valid choices with logging.
    - Added uniqueness validation for 'tag_uid' to prevent duplicate entries.
    - Updated 'urls.py' to register DrugViewSet with a basename for better clarity.

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,9 +2,10 @@ from django.urls import path, include # type: ignore
 from rest_framework.routers import DefaultRouter # type: ignore
 from inventory.views import DrugViewSet, LocationViewSet
 from inventory.views.cart_views import CartViewSet
+from inventory.views.drug_viewset import DrugViewSet
 
 router = DefaultRouter()
-router.register(r'drugs', DrugViewSet)
+router.register(r'drugs', DrugViewSet, basename='drug')
 router.register(r'locations', LocationViewSet)
 router.register(r'carts', CartViewSet)
 
